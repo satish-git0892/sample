14dc55c70688293996845258be54a331bc92161d
satish-git0892 <nandurisatish33@gamil.com>
2025-12-16 22:18:59 +0530

    "started learning variables"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static String instvar = " instance_variable";
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        String firstName = "Satish";
+        String lastName = "Kumar";
+        int age = 34;
+        double height = 5.8;
+        boolean isExcited = true;
+        String name = ("This is " + firstName + " " + lastName);
+        String features = (", My age is " + age  + " and "  + "my height is " + height + " inches");
+        String excite = (", Am i excited? : " + isExcited);
+        System.out.println(name + features + excite + instvar) ;
+        System.out.println("printing instance variable for 2nd time: " + instvar);
+        int myInt = 9;
+        double myDouble = myInt;
+        System.out.println(myInt);
+        System.out.println(myDouble);
     }
 }
