758fa280c3316a3268bdcb964bf6eed7c5b74ee9
satish-git0892 <nandurisatish33@gamil.com>
2025-12-29 20:47:52 +0530

    Encapsulation and getters/setters

--- src/Book.java
+++ src/Book.java
@@ -1,17 +1,38 @@
 public class Book {
-    String title;
-    double price;
+    private String title;
+    private double price;
 
     public Book(String title, double price){
-        this.title = title;
-        this.price = price;
-        System.out.println("The price of book: " + title + " is " + price );
+        setTitle(title);
+        setPrice(price);
+        if (getTitle() != null){
+        System.out.println("The price of book: " + getTitle() + " is " + getPrice() );
+    }
+    }
+
+    public void setTitle(String title){
+        if(!title.equals("")){
+            this.title=title;
+        }else{
+            System.out.println("Empty titles are not allowed");
+        }
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setPrice(double price){
+        this.price=price;
+    }
+    public double getPrice(){
+        return this.price;
     }
 }
 class BookStore {
     public static void main(String[] args) {
-        Book Book1 = new Book("Peace", 27.99);
+        Book Book1 = new Book("", 27.99);
         Book Book2 = new Book("War", 29.99);
-        System.out.println("inside BookStore, The price of book: " + Book1.title + " is " + Book1.price );
+        //System.out.println("inside BookStore, The price of book: " + Book1.getTitle() + " is " + Book1.getPrice() );
     }
 }
---
+++ src/Test.java
@@ -0,0 +1,5 @@
+public class Test {
+    public static void main(String[] args){
+        Book book3 =  new Book("Happy",39.99);
+    }
+}
